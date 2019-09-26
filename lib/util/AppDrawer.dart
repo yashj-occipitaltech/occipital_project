45890fcbe4312db95e5b6b7cb2d2a5ea1fd465e6
Yash <theandroiddeveloper.yj@gmail.com>
2019-09-27 00:57:53 +0530

    added logout dialog

--- lib/util/AppDrawer.dart
+++ lib/util/AppDrawer.dart
@@ -4,7 +4,6 @@ import 'package:occipital_tech/screens/ContactScreen.dart';
 import 'package:occipital_tech/screens/HelpScreen.dart';
 import 'package:occipital_tech/screens/SettingsScreen.dart';
 
-
 class AppDrawer extends StatefulWidget {
   @override
   _AppDrawerState createState() => _AppDrawerState();
@@ -14,33 +13,80 @@ class _AppDrawerState extends State<AppDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Center(child: Text('Agrograde')),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.green,
-                ),
-              ),
-              drawerTiles('Home', BottomNavigator(), Icons.home),
-              drawerTiles('Settings', SettingsScreen(),Icons.settings),
-              drawerTiles('Help', HelpScreen(),Icons.help),
-              drawerTiles('Contact us', ContactScreen(),Icons.contact_phone),
-              drawerTiles('Logout', Container(),Icons.account_circle)
-            ],
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Center(child: Text('Agrograde')),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.green,
+            ),
           ),
-        );
+          drawerTiles('Home', BottomNavigator(), Icons.home),
+          drawerTiles('Settings', SettingsScreen(), Icons.settings),
+          drawerTiles('Help', HelpScreen(), Icons.help),
+          drawerTiles('Contact us', ContactScreen(), Icons.contact_phone),
+          ListTile(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Icon(Icons.account_circle),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('Logout')
+                ],
+              ),
+              onTap: ()  {
+                Navigator.pop(context);
+                _showDialog();
+                
+                }),
+        ],
+      ),
+    );
   }
 
-  Widget drawerTiles(String name, Widget screen,IconData iconName,) {
+  Widget drawerTiles(
+    String name,
+    Widget screen,
+    IconData iconName,
+  ) {
     return ListTile(
       title: Row(
         mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[Icon(iconName),SizedBox(width: 10.0,),Text(name)],),
+        children: <Widget>[
+          Icon(iconName),
+          SizedBox(
+            width: 10.0,
+          ),
+          Text(name)
+        ],
+      ),
       onTap: () => Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (context) => screen)),
     );
+  }
+
+  void _showDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Text('Are you sure you want to logout ?'),
+            title: Text('Logout'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Yes'),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text('No'),
+                onPressed: () => Navigator.of(context).pop(),
+              )
+            ],
+          );
+        });
   }
 }
